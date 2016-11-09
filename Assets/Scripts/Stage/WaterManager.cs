f74b59bc553af1d5927f5c28ae7c572d9d01a1a2
윤여송-PC\여송 <yeosong92>
2016-11-09 23:56:23 +0900

    map water sound add

---
+++ Assets/Scripts/Stage/WaterManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaterManager : MonoBehaviour {
+    public GameObject water;
+    // Use this for initialization
+    public bool playerwaterin = false;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTiriggerEnter(Collider col)
+    {
+        if (col.tag == "Player")
+        {
+            water.GetComponent<AudioSource>().Play();
+        }
+    }
+}
