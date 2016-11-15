c25cb97965ad8a4b31f267bff2f33e4c406e253a
윤여송-PC\여송 <yeosong92>
2016-11-15 18:02:54 +0900

    Water out sound update / other tag load

--- Assets/Scripts/Stage/WaterManager.cs
+++ Assets/Scripts/Stage/WaterManager.cs
@@ -7,6 +7,7 @@ public class WaterManager : MonoBehaviour {
     public GameObject[] wateroutsound;
     public GameObject waterUp;
     public GameObject diving;
+    public GameObject waterout;
     
 
     // Use this for initialization
@@ -23,24 +24,27 @@ public class WaterManager : MonoBehaviour {
     public void OnTriggerEnter(Collider col)
     {
         if (col.tag == "Player")
-        {
+        {      
             waterUp.transform.rotation = Quaternion.Euler(180f, 0f, 0f);
             waterUp.GetComponent<UnityStandardAssets.Water.Water>().reflectLayers.value = -257;
             water.GetComponent<AudioSource>().Play();
-            diving.GetComponent<AudioSource>().PlayOneShot(diving.GetComponent<AudioSource>().clip);         
-            wind.GetComponent<AudioSource>().Stop();                
+            diving.GetComponent<AudioSource>().PlayOneShot(diving.GetComponent<AudioSource>().clip);
+            wind.GetComponent<AudioSource>().Stop();
             GameObject.FindWithTag("Player").GetComponent<AudioSource>().volume = 0;
-            for(int i = 0;  i < wateroutsound.Length; i++)
+            for (int i = 0; i < wateroutsound.Length; i++)
             {
                 wateroutsound[i].GetComponent<AudioSource>().Stop();
             }
         }
+        else if (col.tag == "OtherPlayer") { GetComponent<OtherCharacter>().OnWaterEnter(); }
+
         
     }
     public void OnTriggerExit(Collider col)
     {
         if (col.tag == "Player")
         {
+            waterout.GetComponent<AudioSource>().PlayOneShot(waterout.GetComponent<AudioSource>().clip);
             waterUp.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
             waterUp.GetComponent<UnityStandardAssets.Water.Water>().reflectLayers.value = -1;
             water.GetComponent<AudioSource>().Stop();
@@ -50,6 +54,10 @@ public class WaterManager : MonoBehaviour {
             {
                 wateroutsound[i].GetComponent<AudioSource>().Play();
             }
+        }
+        else if(col.tag == "OtherPlayer")
+        {
+            GetComponent<OtherCharacter>().OnWaterLeave();
         }
 
     }
